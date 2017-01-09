95e0d6ce82945a75f90a315c993f320e044ed65f
yanpa <yanpa@YAN-NOTE>
2017-01-09 17:29:50 -0300

    Add project files.

---
+++ BindingPersistExample/MainViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+
+namespace PersistentBindingExample
+{
+    //Para enviar notificações de propriedade alterada, uma classe de modelo
+    //precisa implementar a interface INotifyPropertyChanged e invocar o manipulador de evento PropertyChanged
+    //sempre que uma propriedade for alterada.
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        //Atributos com valores padrão.
+        private string _text = "Sem Texto";
+        private int _selectedItemIndex = -1;
+        private bool _checkBoxChecked = false;
+        public string[] ComboBoxItems { get; set; } = new[] { "Item1", "Item2", "Item3", "Item4" };
+
+        //Manipulador de evento
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        public string Text
+        {
+            get { return _text; }
+            set { _text = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Text")); }
+        }
+
+        public int SelectedItemIndex
+        {
+            get { return _selectedItemIndex; }
+            set { _selectedItemIndex = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("SelectedItemIndex")); }
+        }
+
+        public bool CheckBoxChecked
+        {
+            get { return _checkBoxChecked; }
+            set { _checkBoxChecked = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("CheckBoxChecked")); }
+        }
+
+    } 
+}
---
+++ BindingPersistExample/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PersistentBindingExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const string STATE_FILE_NAME  = "state.txt";
+        private MainViewModel _viewModel;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            MainViewModel model;
+            //Se o arquivo já existir, carrega o arquivo e desserializa.
+            if (File.Exists(STATE_FILE_NAME))
+            {
+                var fileText = File.ReadAllText(STATE_FILE_NAME);
+                model = JsonConvert.DeserializeObject<MainViewModel>(fileText);
+            }
+            //Se não, apenas instancia o modelo, que virá com valores padrão.
+            else
+            {
+                model = new MainViewModel();
+            }
+
+            //Se inscreve no evento PropertyChanged do modelo.
+            //Métodos inscritos neste evento são notificados sempre que um valor qualquer é alterado.
+            model.PropertyChanged += Model_PropertyChanged;
+
+            //Atribui este modelo ao DataContext e ao local _viewModel.
+            this.DataContext = this._viewModel = model;
+        }
+
+        //Método inscrito em PropertyChanged
+        private void Model_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            //Converte _viewModel para json e salva no arquivo pré-configurado//Se o arquivo já existir, este será sobrescrito.
+            File.WriteAllText(STATE_FILE_NAME, JsonConvert.SerializeObject(this._viewModel));
+        }
+    }
+
+    
+}
